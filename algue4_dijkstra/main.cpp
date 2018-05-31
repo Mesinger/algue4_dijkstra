8549b3cd0cfe4863c156b33e4a73c03d87650dc7
matthias <if17b021@technikum-wien.at>
2018-05-31 21:55:22 +0200

    Merge branch 'master' of /Users/matthias/Dropbox/CLionProjects/algue4_dijkstra with conflicts.

--- algue4_dijkstra/main.cpp
+++ algue4_dijkstra/main.cpp
@@ -1,75 +1,10 @@
-
 #include <iostream>
-<<<<<<< HEAD
-#include <string>
-#include <unordered_set>
-#include <map>
-
-
-using namespace std;
-
-struct NodeHasher;
-struct NodeComperator;
-
-class Nodes {
-private:
-	string Stationsname;
-	string Linie;
-	int Entfernung;
-public:
-	Nodes(string StName, string Lin,int Entf) {
-		Stationsname = StName;
-		Linie = Lin;
-		Entfernung = Entf;
-	}
-	~Nodes() {
-	}
-	string getStName() const
-	{
-		return this->Stationsname;
-	}
-	string getLinie() const
-	{
-		return this->Linie;
-	}
-	string getEntfernung() const
-	{
-		return this->Entfernung;
-	}
-	void displayInfo()
-	{
-		cout << " :: " << this->Stationsname << " :: " << this->Linie << " :: " << this->Entfernung << endl;
-	}
-};
-
-
-typedef std::vector<std::vector<Nodes> > adjacency_list_t;
-=======
 #include <fstream>
 #include <vector>
 #include <sstream>
->>>>>>> 06ebc2df0e82fd0d1a8586fc31914674fd8bcb73
 
 int main(int argc, char** argv) {
-	adjacency_list_t adjacency_list(1);//Netz mit 1 Station Initialisieren
-	vector<Nodes> vec;
 
-<<<<<<< HEAD
-	string St, L;
-	int E;
-	St = "Schwedenplatz";
-	L = "U1";
-	E = 5;
-	adjacency_list[0].push_back(Nodes(St, L, E));
-	St = "Stephansplatz";
-	L = "U1";
-	E = 2;
-	adjacency_list[0].push_back(Nodes(St, L, E));
-	St = "Karlsplatz";
-	L = "U1";
-	E = 3;
-	adjacency_list[0].push_back(Nodes(St, L, E));
-=======
 	/*command line arguments*/
 	if(argc != 2){
 		std::cerr << "Invalid arguments" << std::endl;
@@ -96,15 +31,15 @@ int main(int argc, char** argv) {
 
         lines.emplace_back(stations_distances);
 	}
->>>>>>> 06ebc2df0e82fd0d1a8586fc31914674fd8bcb73
 
-	adjacency_list.push_back(vec);	//Neue Station hinzufügen
-	adjacency_list[1].push_back(adjacency_list[0][1]);
-		
-	adjacency_list[0][0].displayInfo();
-	adjacency_list[0][1].displayInfo();
-	adjacency_list[0][2].displayInfo();
-	adjacency_list[1][0].displayInfo();
-	//return EXIT_SUCCESS;
-	return 0;
+	input.close();
+
+	for(auto line : lines){
+		for(auto s : line){
+
+			std::cout << s << std::endl;
+		}
+	}
+
+	return EXIT_SUCCESS;
 }
