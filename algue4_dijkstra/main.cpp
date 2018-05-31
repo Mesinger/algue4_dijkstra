06ebc2df0e82fd0d1a8586fc31914674fd8bcb73
matthias <if17b021@technikum-wien.at>
2018-05-31 21:42:28 +0200

    file input

--- algue4_dijkstra/main.cpp
+++ algue4_dijkstra/main.cpp
@@ -1,8 +1,36 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
 
 int main(int argc, char** argv) {
 
-	std::cout << "hello" << std::endl;
+	/*command line arguments*/
+	if(argc != 2){
+		std::cerr << "Invalid arguments" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	std::ifstream input(argv[1]);
+
+	if(input.fail()){
+		std::cerr << "Invalid file" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	std::string line, sbuf;
+	std::vector<std::vector<std::string>> lines;
+	std::vector<std::string> stations_distances;
+
+	while(getline(input, line)){
+
+		std::stringstream ss(line);
+
+		while (ss >> sbuf)
+			stations_distances.emplace_back(sbuf);
+
+        lines.emplace_back(stations_distances);
+	}
 
 	return EXIT_SUCCESS;
 }
