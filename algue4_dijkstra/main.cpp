05dfdeccd66db0e9cb0c3c429807eed2535a9026
matthias <if17b021@technikum-wien.at>
2018-06-02 12:21:32 +0200

    reducing main overhead

--- algue4_dijkstra/main.cpp
+++ algue4_dijkstra/main.cpp
@@ -6,35 +6,9 @@
 #include <algorithm>
 #include "Node.h"
 #include "stringsplit.h"
+#include "findinmap.h"
 
 using namespace std;
-
-typedef std::vector<std::vector<Node>> adjacency_list_t;
-typedef pair<string, int> TStatPair;
-typedef map<string, int> TStatMap;
-
-//Suchfunktion um Stationen in der Map zu finden
-TStatMap::const_iterator FindPrefix(const TStatMap& map, const string& search_for) {
-	TStatMap::const_iterator i = map.lower_bound(search_for);
-	if (i != map.end()) {
-		const string& key = i->first;
-		if (key.compare(0, search_for.size(), search_for) == 0) // Really a prefix?
-			return i;
-	}
-	return map.end();
-}
-
-//"Debugfunktion" zur Suche
-TStatMap::const_iterator Test(const TStatMap& map, const string& search_for) {
-	auto i = FindPrefix(map, search_for);
-	if (i != map.end())
-	{
-		cout << "Schon da: " << i->first << ", " << i->second << endl;
-		return i;
-	}
-	else
-		return map.end();
-}
 
 int main(int argc, char** argv) {
 	
@@ -51,9 +25,6 @@ int main(int argc, char** argv) {
 		return EXIT_FAILURE;
 	}
 
-	std::string sbuf;
-	std::vector<std::vector<std::string>> linestations;
-    std::vector<std::vector<int>> linedistances;
 	adjacency_list_t adjacency_list;
 	int adjsize = 0;
 	TStatMap statmap;//pair<statname,number adj.list>
@@ -65,6 +36,7 @@ int main(int argc, char** argv) {
 	string stat_next, stat_prev, stat_zw, statraw, searchstring;
 	std::string linenumber;//LinienNummer
 
+    std::string sbuf;
 	while (getline(input, sbuf)) {
 
 		dist_to_next = 0, dist_to_prev = 0;
@@ -115,30 +87,13 @@ int main(int argc, char** argv) {
 		statmap.insert(TStatPair(statraw, adjsize));
 		adjsize++;
 
-		linestations.emplace_back(stations);	//[0][x]->erste Linie Stationen, [1][x]->zweite Linie Stationen,.....
-		linedistances.emplace_back(distances);	//[0][x]->Distanzen erste Linie, [1][x]->Distanzen zweite Linie
-
 		while (adjacency_list.size() > adjsize + 1)//�berfl�ssige (leere) Eintr�ge im Vektor l�schen
 		{
 			adjacency_list.pop_back();
 		}
-
-		linestations.emplace_back(stations);
-		linedistances.emplace_back(distances);
 	}
 
 	input.close();
-
-	//Adjaceny-List Verkn�pfungen erstellen bei selber Station mit mehreren Linien :)
-	//TStatMap::const_iterator it = statmap.begin();
-//	TStatMap::const_iterator there;
-	//TStatMap::const_iterator end = statmap.end();
-	//vector<string>::const_iterator lineIt;
-	//vector<string>::const_iterator lineEnd = LineArray.end();
-//	std::vector<std::vector<Node>>::const_iterator AdjIt;
-//	std::vector<std::vector<Node>>::const_iterator AdjEnd = adjacency_list.end();
-//	std::vector<Node>::const_iterator NodeIt;
-//	std::vector<Node>::const_iterator NodeEnd;
 
 	for(auto it = statmap.begin(); it != statmap.end(); ++it){
 
@@ -177,51 +132,6 @@ int main(int argc, char** argv) {
 			}
 		}
 	}
-
-//	while (it != end)
-//	{
-//		lineIt = LineArray.begin();
-//		string station = it->first.substr(0, it->first.find(':'));
-//		while (lineIt != lineEnd)
-//		{
-//			string str = station + ":" + *lineIt;
-//			there = statmap.find(str);
-//			if (there != end && there != it)
-//			{
-//				//cout << it->second << endl;
-//				adjacency_list[it->second].emplace_back(str,5);
-//			}
-//			lineIt++;
-//		}
-//
-//		it++;
-//	}
-
-
-//	it = statmap.begin();
-//	while (it != end)
-//	{
-//		lineIt = LineArray.begin();
-//		string station = it->first.substr(0, it->first.find(':'));
-//		while (lineIt != lineEnd)
-//		{
-//			string str = station + ":" + *lineIt;
-//			there = statmap.find(str);
-//			if (there != end && there != it)
-//			{
-//				for (auto& row : adjacency_list)
-//				{
-//					for (auto& node : row)
-//					{
-//						if (node.stationname == it->first)
-//							node.appendNeben(str);
-//					}
-//				}
-//			}
-//			lineIt++;
-//		}
-//		it++;
-//	}
 
     for (int i = 0; i < adjacency_list.size(); i++)
         for (int j = 0; j < adjacency_list[i].size(); j++)
@@ -291,8 +201,6 @@ int main(int argc, char** argv) {
     }while(!validstart || !validend);
 
     std::vector<Node> startstations, endstations;
-
-
 
 	//std::cin.get();
 
